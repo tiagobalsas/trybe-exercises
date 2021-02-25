7be9a9a1c06a74befc3713cbf084d43c3df4e528
Tiago Santos <tiago881@gmail.com>
2021-02-25 16:01:04 -0300

    remove uma variável

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_2.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_2.js
@@ -1,7 +1,6 @@
 /* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. */
 const num1 = 1;
 const num2 = 2;
-let numMaior;
 
 if(num1 > num2) {
     console.log(num1 + ' É maior que ' + num2);
