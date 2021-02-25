3774abe92d2f2c6cce49cc0a65234743cf0bbbab
Tiago Santos <tiago881@gmail.com>
2021-02-25 19:26:38 -0300

    retorna true se  o valor de três variáveis for iguais, se pelo menos um valor for diferente

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
@@ -1,10 +1,10 @@
 /* Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário. */
-const a = 15;
-const b = 15;
-const c = 15;
+const a = 9;
+const b = 9;
+const c = 9;
 
-if ((a + b ) / 2 == c && (a + c) / b ) {
-    console.log('Verdadeiro')
+if (a === b && c == a) {
+    console.log(true)
 } else {
-    console.log('Falso')
+    console.log(false)
 }
