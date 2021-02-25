3d7146309b29263160fcb5d0e756506743538c0d
Tiago Santos <tiago881@gmail.com>
2021-02-25 16:41:09 -0300

    resolve parte do exercício quesito 5

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
@@ -0,0 +1,10 @@
+/* Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário. */
+const a = 15;
+const b = 15;
+const c = 15;
+
+if ((a + b ) / 2 == c && (a + c) / b ) {
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}
