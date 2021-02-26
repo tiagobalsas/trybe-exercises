5fd291b7eaf2ad8b05344d3150b11647c0dfe5ee
Tiago Santos <tiago881@gmail.com>
2021-02-25 21:41:24 -0300

    corrige o script com base no enuciado, mas tem um parte bônus comentada

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
@@ -1,10 +1,31 @@
 /* Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário. */
-const a = 9;
-const b = 9;
-const c = 9;
 
-if (a === b && c == a) {
+
+/* let a = 60;
+let b = 60;
+let c = 60;
+
+if ((a + b + c) / 2 === 90) {
+  console.log(true + ' Retângulo possui um ângulo de 90º');
+} else if ((a + b + c) / 2  < 90) {
+  console.log(true + ' Acutângulo - possui um ângulo menor que 90º');
+} else if ((a + b + c) / 2  > 90) {
+    console.log(true + ' Obtusângulo = possui um ângulo maior que 90º')
+} else {
+    console.log(false)
+} */
+
+let a = 60;
+let b = 60;
+let c = 60;
+
+if (a + b + c === 180) {
     console.log(true)
 } else {
     console.log(false)
 }
+
+
+
+
+
