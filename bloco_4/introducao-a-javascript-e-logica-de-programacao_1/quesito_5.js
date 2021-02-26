73950649e278b6a813bc6408ebd1f6bf85db2c04
Tiago Santos <tiago881@gmail.com>
2021-02-26 08:33:30 -0300

    aplica nova regra de negócio

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_5.js
@@ -1,6 +1,5 @@
 /* Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário. */
 
-
 /* let a = 60;
 let b = 60;
 let c = 60;
@@ -15,7 +14,7 @@ if ((a + b + c) / 2 === 90) {
     console.log(false)
 } */
 
-let a = 60;
+/* let a = 60;
 let b = 60;
 let c = 60;
 
@@ -23,9 +22,37 @@ if (a + b + c === 180) {
     console.log(true)
 } else {
     console.log(false)
+} */
+
+/* 
+if ( a + b + c === 180 ) {
+    if ( ) {
+        
+    } else if (a === b || c != a) {
+        console.log(true + ' Isósceles: possui dois de seus lados com medidas iguais.'
+    } else if (a != b && c != a){
+    }
+    else {
+      console.log(false)
+    }
+  } */
+
+let a = 40;
+let b = 40;
+let c = 100;
+
+if (a + b + c === 180) {
+  if (a === b && c === a) {
+    console.log(
+      true + " - Equilátero: possui todos os lados com mesma medida."
+    );
+  } else if (a === b && c != a) {
+    console.log(
+      true + " - Isósceles: possui dois de seus lados com medidas iguais."
+    );
+  } else if (a != b && c != a) {
+    console.log(true + " - Escaleno: os lados possuem medidas diferentes.");
+  }
+} else {
+  console.log(false);
 }
-
-
-
-
-
