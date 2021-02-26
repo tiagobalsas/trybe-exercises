86e93ded0bb1da6e3f26047c2c13421c6c4981e6
Tiago Santos <tiago881@gmail.com>
2021-02-26 10:59:36 -0300

    cria um programa que verifica se uma de três variáveis possui o valor par, se sim retorna true, se não, false

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_1/quesito_8.js
@@ -0,0 +1,13 @@
+/* Escreva um programa que defina três números em variáveis no seu começo
+ e retorne true se pelo menos uma das três for par. 
+ Caso contrário, ele retorna false . */
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if (num3 % 2 == 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
