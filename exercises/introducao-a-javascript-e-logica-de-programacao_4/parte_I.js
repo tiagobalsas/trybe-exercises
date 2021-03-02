7db0b70a4e63aa5d93796674b81ae72327d4b8ca
Tiago Santos <tiago881@gmail.com>
2021-03-02 09:09:47 -0300

    Cria um novo objeto e concatena os valores com o existente, na sequência imprime no console

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/parte_I.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/parte_I.js
@@ -5,6 +5,13 @@ let info = {
   recorrente: "Sim",
 };
 
+let obj = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
 console.log(`Bem-vinda, ${info.personagem}`);
 console.log(info);
 
@@ -15,3 +22,9 @@ for (prop in info) {
 for (prop in info) {
   console.log(info[prop]);
 }
+
+console.log(`
+${info.personagem} e ${obj.personagem}
+${info.origem} e ${obj.origem}
+${info.nota} e ${obj.nota}
+Ambos recorentes: ${info.recorrente}`);
