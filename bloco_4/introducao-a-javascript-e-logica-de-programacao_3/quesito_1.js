c7ff0088c4980fc0d0258a8602b775477517e711
Tiago Santos <tiago881@gmail.com>
2021-03-02 07:59:50 -0300

    adiciona arquivo vazio

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_3/quesito_1.js
@@ -0,0 +1,7 @@
+/* Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo: */
+let n = 5;
+
+for(let index=0; index < n; index ++) {
+    //imprime n asteriscos(desenvolver a lógica)
+}
+
