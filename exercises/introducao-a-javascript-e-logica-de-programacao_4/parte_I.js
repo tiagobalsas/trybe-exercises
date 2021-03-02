d5dab4cc855ccb72725bcbbed8d54031fe2e17ac
Tiago Santos <tiago881@gmail.com>
2021-03-02 08:47:00 -0300

    adiciona um for in e imprime os valores do objeto no console

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/parte_I.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/parte_I.js
@@ -2,12 +2,16 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: 'Sim'
+  recorrente: "Sim",
 };
 
-console.log(`Bem-vinda, ${info.personagem}`)
+console.log(`Bem-vinda, ${info.personagem}`);
 console.log(info);
 
- for (prop in info){
-  console.log(prop)
-} 
+for (prop in info) {
+  console.log(prop);
+}
+
+for (prop in info) {
+  console.log(info[prop]);
+}
