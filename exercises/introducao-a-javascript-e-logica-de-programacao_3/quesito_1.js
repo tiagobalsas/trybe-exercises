0ece0c5c6388baf07ffbcdf929752533004fbce6
Tiago Santos <tiago881@gmail.com>
2021-03-20 22:03:05 -0300

    remove diretório duplicado

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/quesito_1.js
+++
@@ -1,8 +0,0 @@
-/* 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, 
-seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n  */
-
-let linha = 5;
-for (let index = 0; index < linha; index +=1) {
-  let linha_asterisco = "*";
-  console.log(linha_asterisco);
-}
