7d9c7c6d9062ae60ea08f4c77c02935137a8fe14
Tiago Santos <tiago881@gmail.com>
2021-03-25 15:35:23 -0300

    Agradecimento ao Aluno Wandesron Sales - Turma-10 - Tribo-A por me auxiliar na lógica do requisito 2

--- Bloco-7-Introducao-a-javaScript-ES¨-e-testes-unitarios/let-const-arrow_functions-e-template-literals/parte-I.js
+++ Bloco-7-Introducao-a-javaScript-ES¨-e-testes-unitarios/let-const-arrow_functions-e-template-literals/parte-I.js
@@ -16,8 +16,8 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const orderNumbers = (x) => {
   let aux;
-  for (let index = 0; index < oddsAndEvens.length; index++) {
-    for (let indice = 0; indice < oddsAndEvens.length - index; indice++) {
+  for (let index = 0; index < oddsAndEvens.length; index += 1) {
+    for (let indice = 0; indice < oddsAndEvens.length - index; indice += 1) {
       if (oddsAndEvens[indice] > oddsAndEvens[indice + 1]) {
         aux = oddsAndEvens[indice + 1];
         oddsAndEvens[indice + 1] = oddsAndEvens[indice];
