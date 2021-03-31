c75a9a171187d52c754cbc28cb7f132ba07b1544
Tiago Santos <tiago881@gmail.com>
2021-03-31 19:56:31 -0300

    requisito 3

--- Bloco-7-Introducao-a-javaScript-ES¨-e-testes-unitarios/parte-III.js
+++ Bloco-7-Introducao-a-javaScript-ES¨-e-testes-unitarios/parte-III.js
@@ -1,1 +1,6 @@
-const clickCount = document.querySelector("#clickButton");
+const botao = document.querySelector("#clickButton");
+let clickCount = 0;
+botao.addEventListener("click", () => {
+  const box = document.querySelector("#contagem");
+  box.innerHTML = clickCount += 1;
+});
