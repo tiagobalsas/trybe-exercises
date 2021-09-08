5843d94f80615a8e35555f61991ecc750c72624b
Tiago Santos <tiago881@gmail.com>
2021-09-08 20:44:09 -0300

    Divide o valor de  duas variáveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
@@ -2,10 +2,10 @@
  Seu programa deve ter duas variáveis, a e b , definidas no começo com
   os valores que serão operados. Faça programas para: */
 
-  const a = 10;
-  const b = 5;
+const a = 10;
+const b = 5;
 
-  console.log(`Soma: ${a} + ${b} = ${a + b}`);
-  console.log(`Subtração: ${a} - ${b} = ${a - b}`);
-  console.log(`Multiplicação: ${a} * ${b} = ${a * b}`);
-
+console.log(`Soma: ${a} + ${b} = ${a + b}`);
+console.log(`Subtração: ${a} - ${b} = ${a - b}`);
+console.log(`Multiplicação: ${a} * ${b} = ${a * b}`);
+console.log(`Divisão: ${a} / ${b} = ${a / b}`);
