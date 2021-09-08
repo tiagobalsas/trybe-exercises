fca05d00f7d430632d657829b2afdf08bdefe9c0
Tiago Santos <tiago881@gmail.com>
2021-09-08 20:41:38 -0300

    Soma duas variáveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
@@ -1,44 +1,9 @@
-/* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para: */
+/* 1 - Faça cinco programas, um para cada operação aritmética básica.
+ Seu programa deve ter duas variáveis, a e b , definidas no começo com
+  os valores que serão operados. Faça programas para: */
 
-// Adição
-let a = 2;
-let b = 2;
-let soma;
+  const a = 10;
+  const b = 5;
 
-soma = a + b;
-console.log(soma);
-
-// Subtração
-let a = 2;
-let b = 2;
-let resultado;
-
-resultado = a - b;
-console.log(resultado);
-
-// Multiplicação
-
-let a = 2;
-let b = 2;
-let mult;
-
-mult = a * b;
-console.log(mult);
-
-// Divisão
-
-let a = 2;
-let b = 2;
-let div;
-
-div = a / b;
-console.log(div);
-
-// Operador módulo
-
-let a = 5;
-let b = 2;
-let mod = a % b
-
-console.log(mod);
+  console.log(`Soma: ${a} + ${b} = ${a + b}`);
 
