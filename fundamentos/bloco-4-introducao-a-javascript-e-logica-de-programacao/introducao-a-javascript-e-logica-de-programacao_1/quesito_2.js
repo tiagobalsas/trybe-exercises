11be318dd53201b4db1019d3a0fa8b737f1c2140
Tiago Santos <tiago881@gmail.com>
2021-09-08 15:55:13 -0300

    Remove duplicate directory

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/quesito_2.js
+++
@@ -1,9 +0,0 @@
-/* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. */
-const num1 = 1;
-const num2 = 2;
-
-if(num1 > num2) {
-    console.log(num1 + ' É maior que ' + num2);
-} else {
-    console.log(num2 + ' É maior que ' + num1);
-}
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao_1/quesito_9.js
+++
@@ -1,9 +0,0 @@
-/* Escreva um programa que defina três números em variáveis no seu começo
- e retorne true se pelo menos uma das três for ímpar. 
- Caso contrário, ele retorna false . */
-
- let num1 = 1;
- let num2 = 2;
- let num3 = 3;
-
- num1 % 2 !== 0 ? console.log(true) : console.log(false);
