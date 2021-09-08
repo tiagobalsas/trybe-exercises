a38fff510268178bb3e978917c7af19e98853333
Tiago Santos <tiago881@gmail.com>
2021-09-08 20:47:03 -0300

    Retorna o restante inteiro

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_1.js
@@ -9,3 +9,4 @@ console.log(`Soma: ${a} + ${b} = ${a + b}`);
 console.log(`Subtração: ${a} - ${b} = ${a - b}`);
 console.log(`Multiplicação: ${a} * ${b} = ${a * b}`);
 console.log(`Divisão: ${a} / ${b} = ${a / b}`);
+console.log(`Módulo: ${a} % ${b} = ${a % b}`);
