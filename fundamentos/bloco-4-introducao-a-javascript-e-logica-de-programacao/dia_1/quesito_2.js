90bee0a40a35230ecb32afa7a68e595bf40385be
Tiago Santos <tiago881@gmail.com>
2021-09-08 20:55:34 -0300

    Retorna maior de dois números

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_1/quesito_2.js
@@ -1,9 +1,11 @@
-/* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. */
-const num1 = 1;
-const num2 = 2;
+/* Faça um programa que retorne o maior de dois números.
+ Defina no começo do programa duas variáveis com os valores que serão comparados. */
 
-if(num1 > num2) {
-    console.log(num1 + ' É maior que ' + num2);
+const a = 20;
+const b = 21;
+
+if (a > b) {
+  console.log("'a' é maior que 'b'");
 } else {
-    console.log(num2 + ' É maior que ' + num1);
-}
+  console.log("'b' é maior que 'a'");
+};
